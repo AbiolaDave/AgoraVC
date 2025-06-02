dc0731742127b1596975f73423f5d14213dce0b0
Abiola <abioladave24@gmail.com>
2025-06-02 14:17:57 +0100

    optimized code

--- src/useCustomize.tsx
+++ src/useCustomize.tsx
@@ -1,22 +1,11 @@
 import AppBuilder from "@appbuilder/react";
 import { useEffect } from "react";
 
-const BottomBaroverride = (props: any) => {
-  return (
-    <>
-      {props.children}
-      <button>Party! 🎉</button>
-    </>
-  );
-};
-
 const useCustomize = () => {
   useEffect(() => {
     AppBuilder.customize({
       components: {
-        videoCall: {
-          // bottomToolBar: BottomBaroverride,
-        },
+        videoCall: {},
       },
     });
   }, []);
